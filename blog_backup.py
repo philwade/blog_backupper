96b6c7c3302b62b004b5b15599aa423f4e570a62
phil <phil@philwade.org>
2012-03-18 16:40:12 -0400

    Fixes

--- blog_backup.py
+++ blog_backup.py
@@ -8,15 +8,16 @@ from philwadeorg.philblog.models import Post
 posts = Post.objects.all()
 
 for post in posts:
+    print post.title
     file = open("posts/%s.md" % post.websafe_title, 'wr')
     file.write(html2text(post.body))
     file.close()
 
-    data_file = open("posts/%s.json" % post.websafe_title, 'wr')
-    data_file.write(json.JSONEncoder().encode({   "title": post.title,
-                                                "id" : post.id,
-                                                "date" : str(post.pub_date),
-                                                "websafe_title" : post.websafe_title
+    data_file = open("posts/.%s.json" % post.websafe_title, 'wr')
+    data_file.write(json.JSONEncoder().encode({     "title": post.title,
+                                                    "id" : post.id,
+                                                    "date" : str(post.pub_date),
+                                                    "websafe_title" : post.websafe_title
                                                 }))
     data_file.close()
 
